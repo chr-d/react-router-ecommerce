65e98241934efab99aa2f4294a42bd446b37e0be
chr-d <275697199+chr-d@users.noreply.github.com>
2026-09-01 18:43:51 +0200

    add title and favicon

--- app/root.tsx
+++ app/root.tsx
@@ -20,6 +20,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🐮</text></svg>"
+        />
+        <title>amooozon</title>
         <Meta />
         <Links />
       </head>
