c4c1a7b8e8baaec52ad1f2e4d3c3c3c63ab53a1b
chr-d <275697199+chr-d@users.noreply.github.com>
2026-09-01 15:07:29 +0200

    import Category page from previous react project

--- app/routes/category.tsx
+++ app/routes/category.tsx
@@ -1,5 +1,20 @@
+import { Card } from "~/components/shared/Card";
 import type { Route } from "./+types/category";
+import { useFetch } from "~/utils/useFetch";
 
 export default function Category() {
-  return;
+  const { data, loading, error } = useFetch(
+    "https://fakestoreapi.com/products",
+  );
+  const filteredData = data?.filter((item) => item.category === category);
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="my-4 text-2xl capitalize">{category}</h2>
+      <div className="flex flex-wrap justify-center gap-4">
+        {filteredData?.map((item) => (
+          <Card key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
