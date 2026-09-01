b7f9325f91ad4cb54ae4472ea0c33681315d0451
chr-d <275697199+chr-d@users.noreply.github.com>
2026-09-01 15:23:03 +0200

    move fetch into loader

--- app/routes/category.tsx
+++ app/routes/category.tsx
@@ -1,17 +1,24 @@
 import { Card } from "~/components/shared/Card";
 import type { Route } from "./+types/category";
-import { useFetch } from "~/utils/useFetch";
 
-export default function Category() {
-  const { data, loading, error } = useFetch(
-    "https://fakestoreapi.com/products",
+export async function loader({ params }: Route.LoaderArgs) {
+  const category = params.category;
+
+  const res = await fetch(
+    `https://fakestoreapi.com/products/category/${category}`,
   );
-  const filteredData = data?.filter((item) => item.category === category);
+  const data = await res.json();
+
+  return { data, category };
+}
+
+export default function Category({ loaderData }: Route.ComponentProps) {
+  const { data, category } = loaderData;
   return (
     <div className="flex flex-col items-center">
       <h2 className="my-4 text-2xl capitalize">{category}</h2>
       <div className="flex flex-wrap justify-center gap-4">
-        {filteredData?.map((item) => (
+        {data?.map((item) => (
           <Card key={item.id} item={item} />
         ))}
       </div>
